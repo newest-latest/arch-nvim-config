7dd160be385461c8e63212971dff6ab91b429173
che money <cmelendez15@cnm.edu>
2025-09-14 18:19:18 -0600

    neo tree

--- init.lua
+++ init.lua
@@ -5,5 +5,5 @@ vim.cmd [[
 	call plug#end()
 	]]
 require('lspconfig').clangd.setup {}
-
+require('neo-tree').setup{ sources = {'filesystem'}, position = 'left',}
 vim.keymap.set('i', '<C-Space>', '<C-x><C-o>', { noremap = true })
